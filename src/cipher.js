8a4e001a2510d9de33de59147556f3ccf0cd1d60
Estudiante <natyst_26@hotmail.com>
2020-02-18 12:56:26 -0500

    Isabela

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
-const cipher = {
-  // ...
-};
+const cipher = {}
 
-export default cipher;
+cipher.cifrado = (men,num) =>{
+    let offset = parseInt(num);
+    let longitud=men.length;
+    let respuesta='';
+    for(let i= 0;i<longitud;i++){
+      let codigoAsccii=men.charCodeAt(i);
+      if(codigoAsccii === 32){
+        respuesta+=" ";
+      }
+      else{
+        let formula=(codigoAsccii-65+offset)%26+65;
+        respuesta+=String.fromCharCode(formula);
+      }
+    }
+  
+    return respuesta;
+  }
+  cipher.descifrado = (mensaje,numero) =>{
+    let desplazamiento=parseInt(numero);
+    let longitud2=mensaje.length;
+    let solucion='';
+    for(let a=0;a<longitud2;a++){
+      let cAscii=mensaje.charCodeAt(a);
+      if(cAscii===32){
+        solucion+='';
+      }
+      else{
+        let formula2=(cAscii-90-desplazamiento)%26+90;
+        solucion+=String.fromCharCode(formula2);
+      }
+    }
+    return solucion;
+  }
+  export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 import cipher from './cipher.js';
-
 console.log(cipher);
+
+document.getElementById("cifradoPag").style.display="none";
+document.getElementById("descifradoPag").style.display="none";
+
+const botonCifrado = document.getElementById("btnCifrar")
+
+botonCifrado.addEventListener("click",() =>{
+  let valorMensaje= document.getElementById("textMensaje").value;
+  let valorNumero=document.getElementById("inputNumero").value;
+  console.log(valorMensaje);
+  console.log(valorNumero);
+  let mensajeCifrado=cipher.cifrado(valorMensaje,valorNumero);
+  console.log(mensajeCifrado);
+
+  document.getElementById("respuestaEscrita").innerHTML="En un lenguaje secreto, "+valorMensaje+" significa "+mensajeCifrado
+});
+
+
+
+
+const botonDescifrar=document.getElementById("btnDescifrar");
+
+botonDescifrar.addEventListener("click",()=>{
+  let valorMensaje2=document.getElementById("textMensaje2").value;
+  let valorNumero2=document.getElementById("inputNumero2").value;
+  let mensajeDescifrado=cipher.descifrado(valorMensaje2,valorNumero2);
+  console.log(mensajeDescifrado)
+  document.getElementById("respuestaEscrita2").innerHTML="Descifrado "+ valorMensaje2+", significa: "+ mensajeDescifrado
+
+});
+
+
+function aparecerPagCifrado(){
+  document.getElementById("primeraPag").style.display = "none";
+  document.getElementById("cifradoPag").style.display ="block";
+}
+
+const botonMostrarPC= document.getElementById("btn2P").addEventListener("click", aparecerPagCifrado);
+
+function desaparecerPagCifrado(){
+  document.getElementById("primeraPag").style.display="block";
+  document.getElementById("cifradoPag").style.display ="none";
+}
+
+const botonVolver1= document.getElementById("btnVolver").addEventListener("click", desaparecerPagCifrado)
+
+function aparecerPagDescifrado(){
+  document.getElementById("primeraPag").style.display="none";
+  document.getElementById("descifradoPag").style.display ="block";
+}
+
+const botonMostrarPD= document.getElementById("btn3P").addEventListener("click", aparecerPagDescifrado)
+
+function desaparecerPagDescifrado(){
+  document.getElementById("primeraPag").style.display="block";
+  document.getElementById("descifradoPag").style.display ="none";
+}
+
+const botonVolver2= document.getElementById("btnVolver2").addEventListener("click",desaparecerPagDescifrado)
